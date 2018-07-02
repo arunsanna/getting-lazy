99ea6b01354e84f32cdf3c8bb383699128be40e5
Arun Sanna <asanna@calstatela.edu>
2018-07-02 11:10:25 -0400

    updated code

--- get_all_instances.py
+++ get_all_instances.py
@@ -10,13 +10,17 @@ def get_regions():
 #tagvalue = ['arun.sanna']
 
 for region in get_regions():
-    print region
     client = boto3.client('ec2', region_name= region)
     resp = client.describe_instances()
-        #Filters=[{'Name': 'tag:'+tagkey, 'Values': tagvalue}])
-    #print resp
     for m in resp['Reservations']:
         inst = m['Instances']
         for n in inst:
-            print n['InstanceId'], region, n['State']['Name'], n['InstanceType']
-    #        print n['Tags']['Key']
+            #print n
+            status = n['State']['Name']
+            if status == 'stopped':
+                tags = n['Tags']
+                for tag in tags:
+                    tag_name = str.lower(tag['Key'])
+                    if tag_name == 'owner':
+                        tag_value = str.lower(tag['Value'])
+                        print tag['Value'], (n['InstanceId']), n['LaunchTime']
