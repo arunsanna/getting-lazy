685184ffb645bbf148efee2b4b04e8bfb64f8b0e
Arun Sanna <asanna@calstatela.edu>
2018-07-17 12:36:17 -0400

    cleanup and updated code

--- get_regions.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def get_regions():
-    client = boto3.client('ec2')
-    region_response = client.describe_regions()
-    regions = [region['RegionName'] for region in region_response['Regions']]
-    return regions
-
-print get_regions()
--- list_buckets.py
+++
@@ -1,5 +0,0 @@
-import boto3
-s3client = boto3.client('s3')
-list_buckets_resp = s3client.list_buckets()
-for bucket in list_buckets_resp['Buckets']:
-    print bucket['Name']
